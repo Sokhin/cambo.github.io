d22a2f14ac8096278e84be6d43446b1ef01868f4
Sokhin <160526666+Sokhin@users.noreply.github.com>
2024-03-04 14:07:47 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const x = document.querySelector(".hamburgar-bar");
+const y = document.querySelector(".responsive-nav");
+x.addEventListener("click", function () {
+  y.classList.toggle("active");
+});
